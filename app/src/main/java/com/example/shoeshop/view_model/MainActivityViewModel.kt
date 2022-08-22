a256757f3a7805a2f3d03893817545ad834bee80
MhammadAli <Mohammed180726@feng.bu.edu.eg>
2022-08-22 21:01:02 +0200

    complete two way data binding and added comments in xml files for constraint layout and linear layout

--- app/src/main/java/com/example/shoeshop/view_model/MainActivityViewModel.kt
+++ app/src/main/java/com/example/shoeshop/view_model/MainActivityViewModel.kt
@@ -7,6 +7,10 @@ import androidx.lifecycle.ViewModel
 import com.example.shoeshop.models.Shoe
 
 class MainActivityViewModel : ViewModel() {
+    val name = MutableLiveData<String>()
+    val size = MutableLiveData<String>()
+    val company = MutableLiveData<String>()
+    val description = MutableLiveData<String>()
     private var _shoes = MutableLiveData<MutableList<Shoe>>()
 
     val shoes: LiveData<MutableList<Shoe>> get() = _shoes
@@ -27,7 +31,7 @@ class MainActivityViewModel : ViewModel() {
     fun addShoes(name: String, size: String, company: String, description: String) {
         Log.i("Add new shoes", "addShoes:name $name company $company size $size,desc $description")
 
-        _shoes.value?.add(Shoe(name,size,company,description))
+        _shoes.value?.add(Shoe(name, size, company, description))
 
         Log.i("size---->", "addShoes: size of shoes is ${_shoes.value?.size}")
 
